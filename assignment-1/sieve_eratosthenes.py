acbe1c42d46a4dce358cf3ddf3217c8b540a4916
gnu_user <gsoc.student@gmail.com>
2015-01-18 15:41:37 -0500

    Cleaned up code, use a function to return list of primes

--- assignment-1/sieve_eratosthenes.py
+++ assignment-1/sieve_eratosthenes.py
@@ -9,18 +9,21 @@
 # All rights reserved.
 #
 ###############################################################################
-
 from math import ceil, sqrt
-A = [True] * 29
 
-for i in xrange(2, int(ceil(sqrt(30)))+1):
-    if A[i-2]:
-        for k in xrange(0, 30+1):
-            j = i**2 + k*i
-            if j > 30:
-                break
-            A[j-2] = False
 
-for i in xrange(0, len(A)):
-    if A[i]:
-        print i+2
+def sieve(n):
+    """Returns the list of primes using the Sieve of Eratosthenes"""
+    A = [True] * (n-1)
+
+    for i in xrange(2, int(ceil(sqrt(n)))+1):
+        if A[i-2]:
+            for k in xrange(0, n+1):
+                j = i**2 + k*i
+                if j > n:
+                    break
+                A[j-2] = False
+
+    return [i+2 for i in xrange(0, len(A)) if A[i]]
+
+print "List of primes: ", sieve(30)
