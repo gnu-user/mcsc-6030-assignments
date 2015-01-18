2e5dcdafccad15ef2db59d6aa677b3bb8fd9cf81
gnu_user <gsoc.student@gmail.com>
2015-01-18 17:29:39 -0500

    Added support to try and get the most primes if N not provided

--- assignment-1/sieve_eratosthenes.py
+++ assignment-1/sieve_eratosthenes.py
@@ -10,6 +10,8 @@
 #
 ###############################################################################
 from math import ceil, sqrt
+import sys
+from time import time
 
 
 def sieve(n):
@@ -26,4 +28,25 @@ def sieve(n):
 
     return [i+2 for i in xrange(0, len(A)) if A[i]]
 
-print "List of primes: ", sieve(30)
+# Get the n provided if applicable
+n = None
+if len(sys.argv) > 1:
+    n = sys.argv[1]
+
+# Calculate the number of primes provided or best with time limit
+if n is not None:
+    print "List of primes:", sieve(n)
+else:
+    # Continue multiplying the number of primes until it takes about 60 seconds
+    n = 1000
+    run_time = 0
+    primes = []
+
+    while run_time < 60:
+        start_time = time()
+        primes = sieve(n)
+        run_time = time() - start_time
+        n *= 10
+
+    print "Solved primes using Sieve of Eratosthenes up to %d in %.2f seconds!" % (n, run_time)
+    #print "List of primes:", primes
