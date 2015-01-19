7c7b3f1851019810f8dff88b4d1665acae940518
gnu_user <gsoc.student@gmail.com>
2015-01-18 22:30:33 -0500

    Initial implementation of the sieve in C

---
+++ assignment-1/sieve_eratosthenes.c
@@ -0,0 +1,74 @@
+/******************************************************************************
+ *
+ * Sieve of Eratosthenes Implementation.
+ *
+ * Implementation based on the optimized implementation pseudocode provided on
+ * Wikipedia: http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+ * 
+ * Copyright (C) 2014, Jonathan Gillett (100437638)
+ * All rights reserved.
+ *
+ *****************************************************************************/
+ #include <stdlib.h>
+ #include <stdint.h>
+ #include <stdbool.h>
+ #include <stdio.h>
+ #include <math.h>
+
+/*
+ * Allocates the memory for the list of primes are returns
+ * an array containing the list of prime numbers found
+ * using the Sieve of Eratosthenes.
+ */
+void sieve(uint32_t n, uint32_t *primes)
+{
+    // Memory initialized to false with calloc
+    bool *A = calloc(n-1, sizeof(bool));
+
+    for (uint32_t i = 2; i <= (uint32_t) ceil(sqrt((double) n)); ++i)
+    {
+        if (! A[i-2])
+        {
+            uint32_t j = 0;
+            for (uint32_t k = 0; k <= n; ++k)
+            {
+                j = (uint32_t) (pow((double) i, 2.0) + k*i);
+                if (j > n)
+                {
+                    break;
+                }
+                A[j-2] = true;
+            }
+        }
+    }
+
+    for (uint32_t i = 0, j = 0; i < n-1; ++i)
+    {
+        if (! A[i])
+        {
+            primes[j] = i+2;
+            ++j;
+        }
+    }
+    free(A);
+}
+
+int main(void)
+{
+    uint32_t n = 30;
+    // Allocate the memory for the primes found using the prime-counting 
+    // function, pi(x): http://en.wikipedia.org/wiki/Prime-counting_function
+    uint32_t num_primes = ((uint32_t) ceil(n / log((double) n)) + 1);
+    uint32_t *primes = (uint32_t *) calloc(num_primes, sizeof(uint32_t));
+
+    sieve(n, primes);
+
+    for (int i = 0; i < num_primes; ++i)
+    {
+        printf("%d, ", primes[i]);
+    }
+    printf("\n");
+    free(primes);
+
+    return 0;
+}
