7ba1622391fd53a6f2f61c4f83051fe23aadd8f2
gnu_user <gsoc.student@gmail.com>
2015-01-19 11:15:55 -0500

    Added support to parse the CLI arguments in C!
    This was rather annoying to do...

--- assignment-1/sieve_eratosthenes.c
+++ assignment-1/sieve_eratosthenes.c
@@ -13,7 +13,11 @@
  #include <stdint.h>
  #include <stdbool.h>
  #include <stdio.h>
+ #include <ctype.h>
+ #include <string.h>
  #include <math.h>
+ #include <getopt.h>
+
 
 /*
  * Allocates the memory for the list of primes are returns
@@ -53,21 +57,67 @@ void sieve(uint32_t n, uint32_t *primes)
     free(A);
 }
 
-int main(void)
+int main(int argc, char **argv)
 {
+    int c = 0;
+    bool show_primes = true;
     uint32_t n = 30;
     // Allocate the memory for the primes found using the prime-counting 
     // function, pi(x): http://en.wikipedia.org/wiki/Prime-counting_function
     uint32_t num_primes = ((uint32_t) ceil(n / log((double) n)) + 1);
     uint32_t *primes = (uint32_t *) calloc(num_primes, sizeof(uint32_t));
 
-    sieve(n, primes);
+    // Hack to get the first non-option argument N
+    if (argc > 1)
+    {
+        if (isdigit(*argv[1]))
+        {
+            n = (uint32_t) atoi(argv[1]);
+        }
+    }
+
+    while (1)
+    {
+        static struct option long_options[] =
+        {
+            {"print=", required_argument, 0, 0},
+            {0,0,0,0}
+        };
+        int option_index = 0;
+        c = getopt_long (argc, argv, "", long_options, &option_index);
+        
+        if (c == -1)
+          break;
+
+        switch (c)
+        {
+        case 0:
+            if (long_options[option_index].flag != 0)
+            {
+                puts("breaking");
+                break;
+            }
+            if (strcmp(long_options[option_index].name,"print=") == 0)
+            {
+                if (strcmp(optarg, "false") == 0 || strcmp(optarg, "False") == 0)
+                {
+                    show_primes = false;
+                }
+            }
+            break;
+        default:
+            abort ();
+        }
+    }
+
+    /*sieve(n, primes);
 
     for (int i = 0; i < num_primes; ++i)
     {
         printf("%d, ", primes[i]);
     }
-    printf("\n");
+
+    printf("\n");*/
     free(primes);
 
     return 0;
