c24345828e708aa28ab796a576090153cc2ccfac
gnu_user <gsoc.student@gmail.com>
2015-02-28 02:31:02 -0500

    Verified function is working with value of 2 using GSL_FN_EVAL.

---
+++ assignment-3/question-2/function.h
@@ -0,0 +1,4 @@
+#include <math.h>
+/* f(x;\alpha) = \log(\alpha x) / \sqrt{x} 
+   where \alpha is some positive parameter. */
+double f (double x, void * params);
---
+++ assignment-3/question-2/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <gsl/gsl_integration.h>
+#include "function.h"
+
+extern double f (double, void*);
+
+int
+main (void)
+{
+  /* Claim memory needed for adaptive integration */
+  gsl_integration_workspace * w 
+    = gsl_integration_workspace_alloc (1000);
+  
+  /* Construct GSL function object to describe solution */
+  gsl_function F;
+  F.function = &f;
+  double alpha = 1.0;
+  F.params = NULL;
+
+  /* Parameters to control quadrature routine */
+  double a, b, tol_abs, tol_rel;
+  int limit;
+  a = 0.0;
+  b = 1.0;
+  tol_abs = 0;
+  tol_rel = 1.0e-7;
+  limit = 1000;
+
+  // Test that the function is implemented properly
+  double test = GSL_FN_EVAL(&F,2);
+  printf("RESULT: %.18f\n", test);
+
+
+  /* Actual call to quadrature routine */
+  double result, error;
+  gsl_integration_qags (&F, a, b, tol_abs, tol_rel, limit,
+                        w, &result, &error); 
+  double exact_value = -4.0;
+
+  printf ("result          = % .18f\n", result);
+  printf ("exact result    = % .18f\n", exact_value);
+  printf ("estimated error = % .18f\n", error);
+  printf ("actual error    = % .18f\n", 
+           fabs(result - exact_value));
+  printf ("# intervals     = %d\n", (int) w->size);
+
+  /* Return memory allocated for solver */
+  gsl_integration_workspace_free (w);
+
+  return 0;
+}
