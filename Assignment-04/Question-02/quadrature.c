002daf307915e74ea8ccee84ca8e9209b90a5e43
gnu_user <gsoc.student@gmail.com>
2015-03-28 23:21:00 -0400

    Added the implementation of Simpson's Rule

--- Assignment-04/Question-02/quadrature.c
+++ Assignment-04/Question-02/quadrature.c
@@ -24,6 +24,33 @@ double trapezoid (double (*fp)(double), double a, double b, int N)
   return h*total;
 }
 
+double simpson (double (*fp)(double), double a, double b, int N)
+{
+  /*
+     Estimate the integral of f(x) from a to b using Simpson's Rule 
+     with n points.
+
+     Input:
+       f:  the function to integrate
+       a:  left endpoint
+       b:  right endpoint
+       N:  number of points to use
+     Returns:
+       numerical estimate of the integral
+  */
+  double h = (b-a)/(N-1), total = 0.0;
+
+  for (int m = 0; m < N; m++) {
+     double xj = m*h + a;
+     double xc = xj + h/2.0;
+     total += 2.0*fp(xj) + 4.0*fp(xc);
+  }
+  
+  /* Remove the extra values from the total */
+  total -= fp(a) + fp(b) + 4.0*fp(b + h/2.0);
+  return (h/6.0)*total;
+}
+
 void error_table (double (*fp)(double), double a, double b, 
                   int nrows, int nvals[], double int_true,
                   double (*method)(double (*)(double), double, double, int))
--- Assignment-04/Question-02/quadrature.h
+++ Assignment-04/Question-02/quadrature.h
@@ -2,6 +2,7 @@
 #include <math.h>
 
 double trapezoid (double (*fp)(double), double a, double b, int N);
+double simpson (double (*fp)(double), double a, double b, int N);
 void error_table (double (*fp)(double), double a, double b, 
                   int nrows, int nvals[], double int_true,
                   double (*method)(double (*)(double), double, double, int));
