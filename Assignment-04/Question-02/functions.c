f38c728b19b4642ddb0e41c377702254ae680abc
gnu_user <gsoc.student@gmail.com>
2015-03-28 23:23:02 -0400

    Added a cubic test function to verify implementation is 4th-order accurate.

--- Assignment-04/Question-02/functions.c
+++ Assignment-04/Question-02/functions.c
@@ -10,3 +10,14 @@ double f(double x) {
   f_evals[thread_num]++;
   return (1.0 + x*x*x + sin(k*x));
 }
+
+double f2(double x) {
+  int thread_num; 
+  /* track of number of function evaluations by each thread: */
+  thread_num = 0;   /* default for serial mode */
+#ifdef _OPENMP
+  thread_num = omp_get_thread_num();
+#endif
+  f_evals[thread_num]++;
+  return (1.0 + x*x*x);
+}
--- Assignment-04/Question-02/functions.h
+++ Assignment-04/Question-02/functions.h
@@ -4,3 +4,4 @@
 int *f_evals;       /* Pointer for array to store # evaluations of f */
 double k;           /* Wavenumber in sin(k*x) term of function f     */
 double f (double);
+double f2 (double);
