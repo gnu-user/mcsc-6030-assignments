d82597ec295b420adea17a090567043523551660
gnu_user <gsoc.student@gmail.com>
2015-04-05 15:26:18 -0400

    Added debugging to try and resolve bugs

--- Assignment-04/Question-04/mpi_root.c
+++ Assignment-04/Question-04/mpi_root.c
@@ -12,12 +12,13 @@
 // Define process 0 as MASTER
 #define MASTER 0
 // Define the subinterval limit
-#define INTERVAL 10E-11
+#define INTERVAL 10E-3
 // Set the width for the search space of each subinterval
-#define WIDTH 1E6
-// message tags
-#define REQUEST  1
-#define REPLY    2
+#define WIDTH 1E5
+// Message tags
+#define REQUEST 1
+#define REPLY   2
+
 
 double f(double x) {
     return -2.0 + sin(x) + sin(pow(x, 2)) + sin(pow(x, 3))
@@ -27,7 +28,7 @@ double f(double x) {
 
 // Sets the intervals given the bounds and number of subintervals
 void set_intervals(double intervals[], double left, double right, int n) {
-    double h = ((right - left ) / n);
+    double h = ((right - left) / n);
     for (int i = 0; i < n; ++i) {
         intervals[i] = left + h*i;
     }
@@ -39,14 +40,17 @@ void master(int n_proc) {
     double *intervals = (double *) malloc(n_proc*sizeof(double));
     // Divide the interval [0,1] into several subintervals for each process
     set_intervals(intervals, 0.0, 1.0, n_proc);
-    double ANS[4];                 // The result, r value, bounds from processes
+    double ANS[4] = { 0.0 };       // The result, r value, bounds from processes
     double SOL[4] = { HUGE_VAL };  // The final smallest solution, r value, bounds
     int n_sent = 0;
     MPI_Status status;
 
     // Assign to each task an endpoint of one of the subintervals at which
     // the function f should be evaluated
-    for (int i = 1; i <= n_proc; ++i) {
+    // Print intervals
+    printf("INTERVALS: [%f, %f, %f, %f]\n", 
+           intervals[0], intervals[1], intervals[2], intervals[3]);
+    for (int i = 1; i < n_proc; ++i) {
         double bounds[2] = { intervals[i-1], intervals[i] };
         MPI_Send(bounds, 2, MPI_DOUBLE, i, REQUEST, MPI_COMM_WORLD);
         n_sent++;
@@ -54,11 +58,12 @@ void master(int n_proc) {
 
     while (n_sent > 0) {
         // Receive the r value and result that closest approximates function f(x)
-        MPI_Recv(ANS, 4, MPI_DOUBLE, MPI_ANY_SOURCE, REQUEST,
+        MPI_Recv(ANS, 4, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG,
                  MPI_COMM_WORLD, &status);
 
         // Analyse the function values determined by each process to find a new
         // subinterval [a,b]c[0,1] within the solution r is guaranteed to lie
+        printf("ANS: %f, %f  SOL: %f, %f\n", ANS[0], ANS[1], SOL[0], SOL[1]);
         if (fabs(ANS[0]) <= fabs(SOL[0])) {
             SOL[0] = ANS[0];
             SOL[1] = ANS[1];
@@ -66,35 +71,37 @@ void master(int n_proc) {
             SOL[3] = ANS[3];
             // Update the subintervals for the search space
             set_intervals(intervals, ANS[2], ANS[3], n_proc);
+            printf("INTERVALS: [%f, %f, %f, %f]\n", 
+                   intervals[0], intervals[1], intervals[2], intervals[3]);
         }
 
         // Terminate when the subinterval width is reduced below 10E-11
         if ((ANS[3] - ANS[2]) / WIDTH <= INTERVAL) {
             double bounds[2] = { -1.0, -1.0 };
-            MPI_Send(bounds, 2, MPI_DOUBLE, status.MPI_SOURCE, WIDTH, MPI_COMM_WORLD);
+            MPI_Send(bounds, 2, MPI_DOUBLE, status.MPI_SOURCE, REQUEST, MPI_COMM_WORLD);
             n_sent--;
         }
         // Divide subinterval into smaller pieces for execution again
         else {
             double bounds[2] = { intervals[status.MPI_SOURCE-1],
                                  intervals[status.MPI_SOURCE] };
-            MPI_Send(bounds, 2, MPI_DOUBLE, status.MPI_SOURCE, WIDTH, MPI_COMM_WORLD);
+            MPI_Send(bounds, 2, MPI_DOUBLE, status.MPI_SOURCE, REQUEST, MPI_COMM_WORLD);
         }
     }
     free(intervals);
-    printf("Approx zero of function f(x): %1.15g\nr value: %1.15g", SOL[0], SOL[1]);
+    printf("Approx zero of function f(x): %1.15g\nr value: %1.15g\n", SOL[0], SOL[1]);
 }
 
 
 // The slave process receives the intervals to calculate and the width
 void slave(int proc_id) {
-    double bounds[2];
+    double bounds[2] = { 0.0 };
     double ans[4] = { HUGE_VAL };
-    double h, r, result;
+    double h = 0.0, r = 0.0, result = 0.0;
     MPI_Status status;
-    MPI_Recv(bounds, 2, MPI_DOUBLE, MPI_ANY_SOURCE, REQUEST,
+    MPI_Recv(bounds, 2, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG,
              MPI_COMM_WORLD, &status);
-
+    printf("BOUNDS: [%f, %f]\n", bounds[0], bounds[1]);
     while (bounds[0] >= 0) {
         h = (bounds[1] - bounds[0]) / WIDTH;
         // Given the width iterate through each value of r
@@ -102,7 +109,7 @@ void slave(int proc_id) {
         for (int i = 0; i < WIDTH; ++i) {
             r = bounds[0] + i*h;
             result = f(r);
-            if (ans[0] >= fabs(result)) {
+            if (fabs(ans[0]) >= fabs(result)) {
                 ans[0] = result;
                 ans[1] = r;
                 ans[2] = bounds[0];
@@ -110,9 +117,11 @@ void slave(int proc_id) {
             }
         }
         // Return the results to master process and wait for more work
-        MPI_Send(ans, 4, MPI_DOUBLE, status.MPI_SOURCE, REPLY, MPI_COMM_WORLD);
-        MPI_Recv(bounds, 2, MPI_DOUBLE, MPI_ANY_SOURCE, REQUEST, 
+        MPI_Send(ans, 4, MPI_DOUBLE, status.MPI_SOURCE, REPLY, 
+                 MPI_COMM_WORLD);
+        MPI_Recv(bounds, 2, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, 
                  MPI_COMM_WORLD, &status);
+        printf("BOUNDS: [%f, %f]\n", bounds[0], bounds[1]);
     }
 }
 
@@ -123,16 +132,13 @@ int main(int argc, char *argv[]) {
 
     // Initialize MPI
     MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &proc_id);
     MPI_Comm_size(MPI_COMM_WORLD, &n_proc);
-    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Comm_rank(MPI_COMM_WORLD, &proc_id);
 
     if (proc_id == MASTER) {
-        puts("Master!");
         master(n_proc);
     }
     else {
-        puts("Slave!");
         slave(proc_id);
     }
 
